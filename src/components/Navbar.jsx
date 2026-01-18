392cbed38db5a7a631253b15e95f06aa41f3ab25
Omwami-dev <barasaowabarasa@gmail.com>
2026-01-18 18:34:15 +0300

    update pages

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        ...
+      </Routes>
+    </Router>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,13 @@
 export default function Home() {
   return (
     <div>
+      <nav>
+        <a href="/">Home</a>
+        <a href="/blog">Blog</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+      </nav>
+
       <h1>Omwami Blog</h1>
       <p>Thoughts, Learning & Culture</p>
     </div>
